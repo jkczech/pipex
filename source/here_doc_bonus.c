0130190e4285b6bcf5e4f7605c4c3fab3e2408b7
Josef Koupý <josef.koupy@seznam.cz>
2023-12-17 03:42:20 +0100

    here_doc !

--- source/here_doc_bonus.c
+++ source/here_doc_bonus.c
@@ -6,24 +6,44 @@
 /*   By: jkoupy <jkoupy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/17 00:51:53 by jkoupy            #+#    #+#             */
-/*   Updated: 2023/12/17 02:57:09 by jkoupy           ###   ########.fr       */
+/*   Updated: 2023/12/17 03:40:26 by jkoupy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
 
-//opens infile and outfile, shows errors respectively
+void    here_doc(t_pipex *pipex)
+{
+    char    *buf;
+
+    while (1)
+	{
+		write(1, "pipex heredoc> ", 15);
+        buf = get_next_line(0);
+		if (!buf)
+			exit(1);
+		if (ft_strncmp(pipex->argv[2], buf, ft_strlen(pipex->argv[2])) == 0)
+			break ;
+		write(pipex->infile, buf, ft_strlen(buf) - 1);
+		write(pipex->infile, "\n", 1);
+		free(buf);
+	}
+    free(buf);
+}
+
+//opens infile and outfile like with << and >>
 void	open_here_doc(t_pipex *pipex)
 {
-	pipex->infile = open(pipex->argv[1], O_RDONLY);
+	pipex->infile = open(".here_doc", O_CREAT | O_WRONLY | O_TRUNC, 0000644);
+	if (pipex->infile == -1)
+		error_message(NULL);
+    here_doc(pipex);
+	close(pipex->infile);
+	pipex->infile = open(".here_doc", O_RDONLY);
 	if (pipex->infile == -1)
 	{
-		if (access(pipex->argv[1], F_OK) != 0)
-			error_message(pipex->argv[1]);
-		else if (access(pipex->argv[1], R_OK) != 0)
-			error_message(pipex->argv[1]);
-		else
-			ft_putstr_fd("Error: infile undefined\n", 2);
+		unlink(".here_doc");
+		error_message(NULL);
 	}
 	pipex->outfile = open(pipex->argv[pipex->size + 2],
 			O_WRONLY | O_CREAT | O_APPEND, 0777);
