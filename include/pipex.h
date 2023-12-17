85391eb9a39cd5e8c60c6707f2df25ba94922efc
jkoupy <josef.koupy@seznam.cz>
2023-12-17 18:09:09 +0100

    aligning .h

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: jkoupy <jkoupy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 14:27:19 by jkoupy            #+#    #+#             */
-/*   Updated: 2023/12/17 13:41:05 by jkoupy           ###   ########.fr       */
+/*   Updated: 2023/12/17 18:08:51 by jkoupy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,32 +28,6 @@
 
 # include "structures.h"
 
-//main.c or main_bonus.c
-
-int		main(int argc, char **argv, char **envp);
-bool	parse_input(t_pipex *pipex);
-
-//pipex.c
-
-bool	create_pipes(t_pipex *pipex);
-bool	wait_pids(t_pipex *pipex);
-bool	execute(t_pipex *pipex);
-bool	allocate_pids(t_pipex *pipex);
-bool	pipex_init(t_pipex *pipex, int argc, char **argv, char **envp);
-
-//parse.c
-
-bool	is_command(t_pipex *pipex, char *command, int i);
-void	find_command(t_pipex *pipex, int i);
-bool	find_paths(t_pipex *pipex);
-void	open_files(t_pipex *pipex);
-
-//free.c
-
-bool	free_pipex(t_pipex *pipex);
-bool	free_array(char **array);
-bool	close_pipes(t_pipex *pipex);
-
 //child.c
 
 void	redirect(t_pipex pipex, int input, int output);
@@ -65,8 +39,35 @@ void	child(t_pipex pipex, int i, int input, int output);
 void	error_message(char *file);
 void	cmd_not_found(t_pipex *pipex, int i);
 
+//free.c
+
+bool	close_pipes(t_pipex *pipex);
+bool	free_pipex(t_pipex *pipex);
+bool	free_array(char **array);
+
 //here_doc_bonus.c
 
 void	open_here_doc(t_pipex *pipex);
+void	here_doc(t_pipex *pipex);
+
+//main.c or main_bonus.c
+
+bool	parse_input(t_pipex *pipex);
+int		main(int argc, char **argv, char **envp);
+
+//parse.c
+
+bool	is_command(t_pipex *pipex, char *command, int i);
+void	find_command(t_pipex *pipex, int i);
+bool	find_paths(t_pipex *pipex);
+void	open_files(t_pipex *pipex);
+
+//pipex.c
+
+bool	create_pipes(t_pipex *pipex);
+bool	wait_pids(t_pipex *pipex);
+bool	allocate_pids(t_pipex *pipex);
+bool	execute(t_pipex *pipex);
+bool	pipex_init(t_pipex *pipex, int argc, char **argv, char **envp);
 
 #endif
