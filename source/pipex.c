61c13e5e459a2069a235bb75400898ef06b4dd39
jkoupy <josef.koupy@seznam.cz>
2024-01-17 10:17:44 +0100

    exitcode, removed status

--- source/pipex.c
+++ source/pipex.c
@@ -6,7 +6,7 @@
 /*   By: jkoupy <jkoupy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 11:34:49 by jkoupy            #+#    #+#             */
-/*   Updated: 2024/01/16 15:35:00 by jkoupy           ###   ########.fr       */
+/*   Updated: 2024/01/17 10:16:52 by jkoupy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,25 +35,18 @@ bool	create_pipes(t_pipex *pipex)
 }
 
 //waiting for all the child processes to finish
+//setting exitcode depending on last process
 bool	wait_pids(t_pipex *pipex)
 {
 	int	i;
-	int	status;
 
 	i = 0;
-	status = 0;
 	while (i < pipex->size && pipex->child_pids[i] > 0)
 	{
-		waitpid(pipex->child_pids[i], &status, 0);
+		waitpid(pipex->child_pids[i], NULL, 0);
 		i++;
 	}
-	if (WIFEXITED(status))
-	{
-		pipex->exitcode = WEXITSTATUS(status);
-		if (!pipex->cmds[i - 1].found)
-			pipex->exitcode = 127;
-	}
-	if (pipex->outfile = -1)
+	if (pipex->outfile == -1)
 		pipex->exitcode = 1;
 	else if (!pipex->cmds[i - 1].found)
 		pipex->exitcode = 127;
